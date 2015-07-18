45f36946f626b39cfc672f9e850097fc2e97aa95
xiuweikang <627511996@qq.com>
2015-07-19 00:06:12 +0800

    添加图片和StartActivity

---
+++ src/com/sdust/zhihudaily/activity/GuiderActivity.java
@@ -0,0 +1,38 @@
+package com.sdust.zhihudaily.activity;
+
+import com.example.zhihudaily.R;
+import com.sdust.zhihudaily.fragment.StartFragment;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+
+
+public class GuiderActivity extends Activity {
+	
+	private StartFragment startFragment;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_guider);
+		
+		start();
+	}
+	
+	private void start() {
+		startFragment = new StartFragment();
+		FragmentManager fragmentManager = getFragmentManager();
+		FragmentTransaction transaction = fragmentManager.beginTransaction();
+		transaction.add(R.id.container,startFragment, StartFragment.TAG);
+		transaction.commit();
+	}
+	@Override
+    public void onAttachFragment(Fragment fragment) {
+      
+    }
+
+}
---
+++ src/com/sdust/zhihudaily/fragment/StartFragment.java
@@ -0,0 +1,27 @@
+package com.sdust.zhihudaily.fragment;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class StartFragment extends Fragment{
+	
+	private TextView mAuthorView;
+	private ImageView mLogoImg;
+	private ImageView mStartImg;
+	
+	
+	public static final String TAG = StartFragment.class.getSimpleName();
+	
+	@Override
+	public void onAttach(Activity activity) {
+		// TODO Auto-generated method stub
+		super.onAttach(activity);
+
+		
+	}
+	
+	
+
+}
