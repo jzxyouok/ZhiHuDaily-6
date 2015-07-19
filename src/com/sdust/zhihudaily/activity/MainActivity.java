263f25afe05468211e51d3f21870d22eb834e233
xiuweikang <627511996@qq.com>
2015-07-19 20:39:24 +0800

    添加MainActivity

---
+++ src/com/sdust/zhihudaily/activity/MainActivity.java
@@ -0,0 +1,14 @@
+/**
+ * 文件名：MainActivity.java
+ * 时间：2015年7月19日下午8:38:59
+ * 作者：Kevin
+ */
+package com.sdust.zhihudaily.activity;
+
+/**
+ * 类名：MainActivity
+ * 说明：
+ */
+public class MainActivity {
+
+}
