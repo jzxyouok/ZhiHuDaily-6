210501337f2b7d92e8b48449ba8e49234d8883b4
xiuweikang <627511996@qq.com>
2015-08-07 22:14:19 +0800

    fix bug

--- app/src/main/java/com/sdust/zhihudaily/fragment/DailyStoriesFragment.java
+++ app/src/main/java/com/sdust/zhihudaily/fragment/DailyStoriesFragment.java
@@ -1,7 +1,86 @@
 package com.sdust.zhihudaily.fragment;
 
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sdust.zhihudaily.R;
+import com.sdust.zhihudaily.adapter.DailyStoriesAdapter;
+import com.sdust.zhihudaily.widget.LoadMoreRecyclerView;
+
+
 public class DailyStoriesFragment extends BaseFragment {
     public static final String TAG = DailyStoriesFragment.class.getSimpleName();
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private DailyStoriesAdapter mAdapter;
+    private LoadMoreRecyclerView mRecyclerView;
+    private LinearLayoutManager mLayoutManager;
+    private String mDate;
 
+    private boolean isDataLoaded;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAdapter = new DailyStoriesAdapter();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_daily_stories,container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView = (LoadMoreRecyclerView)view.findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setOnLoadMoreListener(new LoadMoreRecyclerView.onLoadMoreListener() {
+            @Override
+            public void onLoadMore() {
+                loadMore();
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                changeActionBarTitle(dy);
+            }
+        });
+        mSwipeRefreshLayout = (SwipeRefreshLayout)view.findViewById(R.id.swipeRefreshLayout);
+        mSwipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_dark, android.R.color.holo_blue_light, android.R.color.holo_green_light, android.R.color.holo_green_light);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refresh();
+            }
+        });
+    }
+    @Override
+    public void onResume() {
+
+    }
+
+    @Override
+    public void onPause() {
+
+    }
+    private String mTitle;
+    private int lastTitlePos = -1;
+    private void changeActionBarTitle(int dy) {
+
+    }
+
+    private void loadMore() {
+
+    }
+
+    private void refresh() {
+
+    }
 
 }
