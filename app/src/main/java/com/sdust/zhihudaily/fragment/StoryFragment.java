5f64bb7c8cc719785026fc4cef6c27aac368f268
xiuweikang <627511996@qq.com>
2015-08-10 16:01:33 +0800

    添加ScroPullDownHelper

--- app/src/main/java/com/sdust/zhihudaily/fragment/StoryFragment.java
+++ app/src/main/java/com/sdust/zhihudaily/fragment/StoryFragment.java
@@ -3,6 +3,23 @@ package com.sdust.zhihudaily.fragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.sdust.zhihudaily.R;
+import com.sdust.zhihudaily.model.Story;
+import com.sdust.zhihudaily.widget.AvatarsView;
+import com.sdust.zhihudaily.widget.ScrollPullDownHelper;
+import com.sdust.zhihudaily.widget.StoryHeaderView;
+
+import java.lang.ref.SoftReference;
+
+import butterknife.InjectView;
 
 /**
  * Created by Kevin on 2015/8/10.
@@ -10,7 +27,35 @@ import android.support.v7.widget.Toolbar;
 public class StoryFragment extends Fragment {
     public static final String TAG = StoryFragment.class.getSimpleName();
 
+    @InjectView(R.id.pb)
+    ProgressBar progressBar;
+
+    @InjectView(R.id.rl_container_header)
+    RelativeLayout rlStoryHeader;
+
+    @InjectView(R.id.scrollView)
+    ScrollView scrollView;
+
+    @InjectView(R.id.webViewContainer)
+    LinearLayout llWebViewContainer;
+
+    @InjectView(R.id.avatarsView)
+    AvatarsView avatarsView;
+
+    @InjectView(R.id.spaceView)
+    View spaceView;
+
+    private SoftReference<WebView> refWebView;
+
+    private StoryHeaderView storyHeaderView;
+
+    private DisplayImageOptions mOptions;
+
+    private ScrollPullDownHelper mScrollPullDownHelper;
+
     private String mStoryId;
+
+    private Story mStory;
     private Toolbar mActionBarToolbar;
 
     public StoryFragment() {
@@ -32,6 +77,21 @@ public class StoryFragment extends Fragment {
     public void setToolBar(Toolbar toolbar) {
         this.mActionBarToolbar = toolbar;
     }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        if (getArguments() != null) {
+            mStoryId = getArguments().getString(DailyStoriesFragment.EXTRA_STORY_ID);
+        }
 
+        mScrollPullDownHelper = new ScrollPullDownHelper();
+
+        this.mOptions = new DisplayImageOptions.Builder()
+                .cacheInMemory(false)
+                .cacheOnDisk(true)
+                .considerExifParams(true)
+                .build();
+    }
 
 }
