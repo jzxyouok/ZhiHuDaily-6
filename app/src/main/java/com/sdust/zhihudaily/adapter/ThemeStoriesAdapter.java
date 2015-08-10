ec691d279a99925909b3f0b96c14d83a14f1518b
xiuweikang <627511996@qq.com>
2015-08-10 15:08:17 +0800

    完成ThemeStoriesAdapter

--- app/src/main/java/com/sdust/zhihudaily/adapter/ThemeStoriesAdapter.java
+++ app/src/main/java/com/sdust/zhihudaily/adapter/ThemeStoriesAdapter.java
@@ -2,12 +2,20 @@ package com.sdust.zhihudaily.adapter;
 
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.sdust.zhihudaily.R;
+import com.sdust.zhihudaily.adapter.holder.StoryViewHolder;
+import com.sdust.zhihudaily.model.Editor;
 import com.sdust.zhihudaily.model.Story;
 import com.sdust.zhihudaily.model.Theme;
+import com.sdust.zhihudaily.util.DensityUtil;
+import com.sdust.zhihudaily.widget.AvatarsView;
+import com.sdust.zhihudaily.widget.StoryHeaderView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,15 +23,15 @@ import java.util.List;
  */
 public class ThemeStoriesAdapter extends RecyclerView.Adapter {
 
-    public static final class Type{
+    public static final class Type {
         public static final int TYPE_HEADER = 0;
-        
+
         public static final int TYPE_AVATARS = 1;
-        
+
         public static final int TYPE_ITEM = 2;
     }
-    
-    
+
+
     @Override
     public int getItemViewType(int position) {
         if (!TextUtils.isEmpty(mTheme.getBackground()) && position == 0) {
@@ -34,17 +42,17 @@ public class ThemeStoriesAdapter extends RecyclerView.Adapter {
             return Type.TYPE_ITEM;
         }
     }
-    
+
     private Theme mTheme;
-    
+
     public ThemeStoriesAdapter() {
     }
-    
+
     public void setTheme(Theme theme) {
         mTheme = theme;
         notifyDataSetChanged();
     }
-    
+
     public void appendStories(List<Story> stories) {
         mTheme.getStories().addAll(stories);
         notifyDataSetChanged();
@@ -52,12 +60,48 @@ public class ThemeStoriesAdapter extends RecyclerView.Adapter {
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        RecyclerView.ViewHolder viewHolder = null;
+        switch (viewType) {
+            case Type.TYPE_HEADER:
+                viewHolder = new HeaderViewHolder(StoryHeaderView.newInstance(parent));
+                break;
+            case Type.TYPE_AVATARS:
+                AvatarsView avatarsView = new AvatarsView(parent.getContext());
+                RecyclerView.LayoutParams lp = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                lp.setMargins(0, DensityUtil.dipToPx(parent.getContext(), 8), 0, 0);
+                avatarsView.setLayoutParams(lp);
+                viewHolder = new AvatarViewHolder(avatarsView);
+                break;
+            case Type.TYPE_ITEM:
+                viewHolder = new StoryViewHolder(LayoutInflater.from(parent.getContext()).
+                        inflate(R.layout.recycler_item_story, parent, false));
+                break;
+        }
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+        int viewType = getItemViewType(position);
+        switch (viewType) {
+            case Type.TYPE_HEADER:
+                ((StoryHeaderView) holder.itemView)
+                        .bindData(mTheme.getDescription(), null, mTheme.getBackground());
+                break;
+            case Type.TYPE_AVATARS:
+                List<String> images = new ArrayList<>();
+                for (Editor editor : mTheme.getEditors()) {
+                    images.add(editor.getAvatar());
+                }
+                ((AvatarsView) holder.itemView).bindData(holder.itemView.getResources().getString(R.string.avatar_title_editor), images);
+                break;
+            case Type.TYPE_ITEM:
+                StoryViewHolder storyViewHolder = (StoryViewHolder) holder;
+                storyViewHolder.bindStoryView(mTheme.getStories().get(position - 2));
+                break;
+            default:
+                throw new IllegalArgumentException("error view type!");
+        }
     }
 
     @Override
@@ -76,6 +120,7 @@ public class ThemeStoriesAdapter extends RecyclerView.Adapter {
         }
         return count;
     }
+
     public static final class HeaderViewHolder extends RecyclerView.ViewHolder {
         public HeaderViewHolder(View itemView) {
             super(itemView);
